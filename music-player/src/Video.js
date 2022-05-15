662d5c082e2b9984b149ecb33fc329708a5c8212
preeti003 <k.preeti1002@gmail.com>
2022-05-15 22:26:54 +0530

    sliding window

--- music-player/src/Video.js
+++ music-player/src/Video.js
@@ -1,8 +1,16 @@
 import React, {useRef} from "react";
 import "./Video.css";
+import IMAGE from ".//IMG.jpg";
+import Song from ".//song.mp3"
 
-<h1>Hello</h1>
 
+class MyVideoComponent extends React.Component {
+  render() {
+    return (
+     <img src={IMAGE} ></img> 
+     <audio src={Song} autoplay ></audio>
+    );
+  }
+}
 
-
-export default Video
+export default MyVideoComponent
